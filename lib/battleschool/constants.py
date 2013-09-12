c713acefc8f8e680f536eaa8cf27d96b5bb1c4c5
Spencer Gibb <spencer@gibb.us>
2013-09-12 16:08:18 -0600

    added support for default hosts location

--- lib/battleschool/constants.py
+++ lib/battleschool/constants.py
@@ -15,8 +15,11 @@ def get_config(env_var, default):
 # correct location. See #1277 for discussion
 if getattr(sys, "real_prefix", None):
     DIST_MODULE_PATH = os.path.join(sys.prefix, 'share/battleschool/')
+    DIST_CONFIG_PATH = os.path.join(sys.prefix, 'etc/battleschool/')
 else:
     DIST_MODULE_PATH = '/usr/share/battleschool/'
+    DIST_CONFIG_PATH = '/etc/battleschool/'
 
 DEFAULT_MODULE_PATH = get_config('BATTLESCHOOL_LIBRARY', DIST_MODULE_PATH)
+DEFAULT_HOST_LIST = os.path.expanduser(get_config('BATTLESCHOOL_HOSTS', os.path.join(DIST_CONFIG_PATH, 'hosts')))
 
