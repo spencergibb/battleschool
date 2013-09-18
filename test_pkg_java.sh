0b72a4980753bfdd2d79885123b12c772ad5dcb5
Spencer Gibb <spencer@gibb.us>
2013-09-17 20:04:43 -0600

    added local src support in mac_pkg; added mac_version and mac_major_minor_version

--- test_pkg_java.sh
+++ test_pkg_java.sh
@@ -1,9 +1,9 @@
 MODULE_PARAMS="name=com.oracle.jdk7u25"
 MODULE_PARAMS="$MODULE_PARAMS state=present"
-MODULE_PARAMS="$MODULE_PARAMS required_version=1.1x"
+MODULE_PARAMS="$MODULE_PARAMS required_version=1.1"
 MODULE_PARAMS="$MODULE_PARAMS dest=/tmp/jdk7.dmg"
 MODULE_PARAMS="$MODULE_PARAMS cookie=gpw_e24=http%3A%2F%2Fwww.oracle.com"
-MODULE_PARAMS="$MODULE_PARAMS url=https://edelivery.oracle.com/otn-pub/java/jdk/7u25-b15/jdk-7u25-macosx-x64.dmg"
+#MODULE_PARAMS="$MODULE_PARAMS url=https://edelivery.oracle.com/otn-pub/java/jdk/7u25-b15/jdk-7u25-macosx-x64.dmg"
 MODULE_PARAMS="$MODULE_PARAMS dmg_package='JDK 7 Update 25.pkg'"
 #echo $MODULE_PARAMS
-~/workspace/32degrees/ansible/hacking/test-module -m library/packaging/mac_pkg -a "$MODULE_PARAMS"
+~/workspace/32degrees/ansible/hacking/test-module -m share/library/mac_pkg -a "$MODULE_PARAMS"
