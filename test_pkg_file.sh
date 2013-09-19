e43ad11ba978566fc80a7a56a635003a2a3c295b
Spencer Gibb <spencer@gibb.us>
2013-09-18 22:55:28 -0600

    fixed src install vs dest download

--- test_pkg_file.sh
+++ test_pkg_file.sh
@@ -1,7 +1,8 @@
-MODULE_PARAMS="name=com.oracle.jdk7u25"
-MODULE_PARAMS="$MODULE_PARAMS state=present"
-MODULE_PARAMS="$MODULE_PARAMS required_version=1.1"
+MODULE_PARAMS="state=present"
+MODULE_PARAMS="$MODULE_PARAMS pkg_name=com.oracle.jdk7u25"
+MODULE_PARAMS="$MODULE_PARAMS pkg_version=1.1"
+MODULE_PARAMS="$MODULE_PARAMS archive_type=dmg"
 MODULE_PARAMS="$MODULE_PARAMS src=/tmp/jdk7.dmg"
-MODULE_PARAMS="$MODULE_PARAMS dmg_package='JDK 7 Update 25.pkg'"
+MODULE_PARAMS="$MODULE_PARAMS dmg_path='JDK 7 Update 25.pkg'"
 #echo $MODULE_PARAMS
 ~/workspace/32degrees/ansible/hacking/test-module -m share/library/mac_pkg -a "$MODULE_PARAMS"
