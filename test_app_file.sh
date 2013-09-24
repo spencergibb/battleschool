57cf14d715d60b770321e00c4e34c8018500e13d
Spencer Gibb <spencer@gibb.us>
2013-09-24 09:25:59 -0600

    added app package support

---
+++ test_app_file.sh
@@ -0,0 +1,7 @@
+MODULE_PARAMS="state=present"
+MODULE_PARAMS="$MODULE_PARAMS src=/tmp/Alfred.zip"
+MODULE_PARAMS="$MODULE_PARAMS pkg_type=app"
+MODULE_PARAMS="$MODULE_PARAMS archive_type=zip"
+MODULE_PARAMS="$MODULE_PARAMS archive_path='Alfred 2.app'"
+#echo $MODULE_PARAMS
+~/workspace/32degrees/ansible/hacking/test-module -m share/library/mac_pkg -a "$MODULE_PARAMS"
