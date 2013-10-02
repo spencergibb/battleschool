5bf7d864dcf4bf34f362f2d3c0aff2c2c6f0fd32
Spencer Gibb <spencer@gibb.us>
2013-10-02 00:05:11 -0600

    auto comput destination if not provided

--- test_pkg_macports.sh
+++ test_pkg_macports.sh
@@ -1,7 +1,8 @@
 MODULE_PARAMS="state=present"
 MODULE_PARAMS="$MODULE_PARAMS pkg_name=org.macports.MacPorts"
 MODULE_PARAMS="$MODULE_PARAMS pkg_version=0.2.2.0.0.0.0.0.0x"
-MODULE_PARAMS="$MODULE_PARAMS dest=/tmp/macports.pkg"
+#MODULE_PARAMS="$MODULE_PARAMS dest=/tmp/macports.pkg"
+MODULE_PARAMS="$MODULE_PARAMS force=true"
 MODULE_PARAMS="$MODULE_PARAMS url=https://distfiles.macports.org/MacPorts/MacPorts-2.2.0-10.8-MountainLion.pkg"
 #echo $MODULE_PARAMS
 ~/workspace/32degrees/ansible/hacking/test-module -m share/library/mac_pkg -a "$MODULE_PARAMS"
