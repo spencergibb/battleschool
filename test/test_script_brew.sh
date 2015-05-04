0e44a42bb37a6ea4ac89a10934d6872959501899
Spencer Gibb <spencer@gibb.us>
2015-05-04 16:19:58 -0600

    added script_postfix and script_data params
    fixes gh-28

--- test/test_script_brew.sh
+++ test/test_script_brew.sh
@@ -1,7 +1,9 @@
 MODULE_PARAMS="pkg_type=script"
 MODULE_PARAMS="$MODULE_PARAMS script_creates=/usr/local/bin/brew"
-MODULE_PARAMS="$MODULE_PARAMS script_prefix='echo | '"
-MODULE_PARAMS="$MODULE_PARAMS script_exe=ruby"
-MODULE_PARAMS="$MODULE_PARAMS url=https://raw.github.com/mxcl/homebrew/go"
+#MODULE_PARAMS="$MODULE_PARAMS script_prefix='yes | '"
+MODULE_PARAMS="$MODULE_PARAMS script_exe=/usr/bin/ruby"
+MODULE_PARAMS="$MODULE_PARAMS url=https://raw.githubusercontent.com/Homebrew/install/master/install"
+#MODULE_PARAMS="$MODULE_PARAMS script_postfix=' < /dev/null'"
+MODULE_PARAMS="$MODULE_PARAMS script_data='\n'"
 #echo $MODULE_PARAMS
 $ANSIBLE_SRC_PATH/hacking/test-module -m share/library/mac_pkg -a "$MODULE_PARAMS"
